f71a9730136d0981fe829bf62eed305a718b7e14
kamranch24 <65529424+kamranch24@users.noreply.github.com>
2024-07-19 13:23:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,43 +2,26 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_eks_cluster" "eks" {
-  name     = "my-cluster"
-  role_arn = aws_iam_role.eks.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.public.*.id
-  }
-}
-
-resource "aws_eks_node_group" "node_group" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = "my-node-group"
-  node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = aws_subnet.public.*.id
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 2
-    min_size     = 1
-  }
-}
-
-resource "aws_iam_role" "eks" {
+resource "aws_iam_role" "eks_cluster" {
   name = "eks-cluster-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "eks.amazonaws.com"
         }
       },
     ]
   })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
 }
 
 resource "aws_iam_role" "node" {
@@ -48,19 +31,14 @@ resource "aws_iam_role" "node" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
         Principal = {
           Service = "ec2.amazonaws.com"
         }
       },
     ]
   })
-}
-
-resource "aws_iam_role_policy_attachment" "eks" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks.name
 }
 
 resource "aws_iam_role_policy_attachment" "node" {
